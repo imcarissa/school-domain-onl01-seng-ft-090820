2e0b831844cd62ffc89248500576e7b387183633
Carissa Sarreal <carissa.sarreal@gmail.com>
2020-09-20 18:50:02 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,33 @@
-# code here!
+require 'pry'
+class School
+  
+  attr_accessor :school, :roster, :grade, :sort
+  
+  def initialize(school)
+    @school = school
+    @roster = {}
+  end
+  
+  def add_student(name, grade)
+   if @roster[grade] 
+      @roster[grade] << name
+   else
+     @roster[grade] = [name]
+   end
+  end
+  
+  def grade(level)
+    @roster[level]
+  end
+
+  def sort (students)
+    @roster = [students]
+      @roster.sort do |name, grade|
+        a <=> b
+  end
+    @roster
+  end
+  
+end
+
+school = School.new("Bayside High School")
